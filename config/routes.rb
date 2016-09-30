27dc5c4a36ea3bafd28707753ef0841469e80a3c
Jesse Tilley <jesseandrewtilley@gmail.com>
2016-09-30 15:33:42 +1000

    Added phone number instead of call

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
   root "pages#home"
 devise_scope :user do
   get "/admin" => "devise/sessions#new"
