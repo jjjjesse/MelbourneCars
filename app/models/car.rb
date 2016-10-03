ed6b09012ec1295d3f3b794e0f69e2b9924469a9
Jesse Tilley <jesseandrewtilley@gmail.com>
2016-10-03 22:54:12 +1100

    Added 'large' style to pics

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :large => "750x750>", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
