c3c3607d917166fb9990df2ec43960f126602a7f
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-15 13:24:55 +1100

    Fix function

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,8 +1,9 @@
 module CarsHelper
     def add_commas(number)
       if number.to_s > 3
-        return number.to_s[0..-2] + "," + number.to_s[-3..-1]
+        @number_out = number.to_s[0..-2] + "," + number.to_s[-3..-1]
       else
-        return number.to_s
+        @number_out = number.to_s
       end
+      @number_out
 end
