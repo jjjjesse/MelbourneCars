5e15f375cc97d1b6b9279ed2c95c97230218aa92
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-15 13:39:57 +1100

    Add commas to prices, fix range

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,7 +1,7 @@
 module CarsHelper
   def add_commas(number)
     if number.to_s.length > 3
-      @number_out = number.to_s[0..-2] + "," + number.to_s[-3..-1]
+      @number_out = number.to_s[0..-4] + "," + number.to_s[-3..-1]
     else
       @number_out = number.to_s
     end
