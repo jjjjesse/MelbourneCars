ede5ca94ba515ab07ca7655a723ee50f476ff08b
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-15 13:37:26 +1100

    Remove semicol, wrong langauge again..

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,6 +1,6 @@
 module CarsHelper
   def add_commas(number)
-    if number.to_s.length > 3:
+    if number.to_s.length > 3
       @number_out = number.to_s[0..-2] + "," + number.to_s[-3..-1]
     else
       @number_out = number.to_s
