a712968379bf005897948c5866fc887f52e3f36f
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-15 13:34:35 +1100

    Fix string to int comp bug

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,10 +1,10 @@
 module CarsHelper
-    def add_commas(number)
-      if number.to_s > 3
-        @number_out = number.to_s[0..-2] + "," + number.to_s[-3..-1]
-      else
-        @number_out = number.to_s
-      end
-      @number_out
+  def add_commas(number)
+    if number.to_s.length > 3:
+      @number_out = number.to_s[0..-2] + "," + number.to_s[-3..-1]
+    else
+      @number_out = number.to_s
     end
+    @number_out
+  end
 end
