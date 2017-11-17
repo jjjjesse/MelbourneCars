0237138bf7d21994f2805da8b99323f1b2792be7
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-17 14:52:44 +1100

    Add sold/unsold to cars model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,7 @@
 class Car < ApplicationRecord
 	belongs_to :user
+  scope :unsold, -> {:sold => false}
+  scope :sold, -> {:sold => true}
 	has_attached_file :image, :styles => { :large => "750x750>", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
