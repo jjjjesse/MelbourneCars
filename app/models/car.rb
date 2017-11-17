21e19ef1dd9a9b368e9e3472111cb133ee3b5c94
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-17 15:08:37 +1100

    Change scope syntax more

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
 	belongs_to :user
-  scope :unsold, where(sold: false)
-  scope :sold, where(sold: true)
+  scope :unsold, -> { where(sold: false) }
+  scope :sold, -> { where(sold: true) }
 	has_attached_file :image, :styles => { :large => "750x750>", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
